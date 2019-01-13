aa951865e17dc50365bf55ef92374dd47c27acb6
MeisterGig <MeisterGig@web.de>
2019-01-14 00:00:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key  | sudo apt-key add -
 echo "deb https://dev2day.de/pms/ stretch main" | sudo tee /etc/apt/sources.list.d/pms.list
 sudo apt-get update
 sudo apt-get install -t stretch plexmediaserver-installer -y
-sudo reboot
+printf "\n--------\nPlex Installation Succsessful!\n"
