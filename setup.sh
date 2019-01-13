f89bc82b0ae1b85501b3c706114963ba750ca92c
MeisterGig <MeisterGig@web.de>
2019-01-13 23:42:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https -y --force-yes
 wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key  | sudo apt-key add -
